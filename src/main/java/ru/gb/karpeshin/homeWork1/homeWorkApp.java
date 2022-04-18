c400d9c950f0f871668a3421a366d850d76fc5f6
MaksimKarpeshin <feap135@yandex.ru>
2022-04-18 04:00:12 +0300

    Task 2

--- src/main/java/ru/gb/karpeshin/homeWork1/homeWorkApp.java
+++ src/main/java/ru/gb/karpeshin/homeWork1/homeWorkApp.java
@@ -1,7 +1,21 @@
 package ru.gb.karpeshin.homeWork1;
 
+/**
+ * Task 1. Создать пустой проект в IntelliJ IDEA, создать класс HomeWorkApp, и прописать в нем метод
+ * main().
+ */
 public class homeWorkApp {
     public static void main(String[] args) {
+        printThreeWords();
+    }
 
+    /**
+     * Task 2. Создайте метод printThreeWords(), который при вызове должен отпечатать в столбец три
+     * слова: Orange, Banana, Apple.
+     */
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
     }
 }
