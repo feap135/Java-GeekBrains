2f694fff8269d8034d8d9901c30c54cc3698cd7a
MaksimKarpeshin <feap135@yandex.ru>
2022-04-18 04:28:48 +0300

    Task 3

--- src/main/java/ru/gb/karpeshin/homeWork1/homeWorkApp.java
+++ src/main/java/ru/gb/karpeshin/homeWork1/homeWorkApp.java
@@ -7,6 +7,7 @@ package ru.gb.karpeshin.homeWork1;
 public class homeWorkApp {
     public static void main(String[] args) {
         printThreeWords();
+        checkSumSign();
     }
 
     /**
@@ -18,4 +19,23 @@ public class homeWorkApp {
         System.out.println("Banana");
         System.out.println("Apple");
     }
+
+    /**
+     * 3. Создайте метод checkSumSign(), в теле которого объявите две int переменные a и b, и
+     * инициализируйте их любыми значениями, которыми захотите. Далее метод должен
+     * просуммировать эти переменные, и если их сумма больше или равна 0, то вывести в консоль
+     * сообщение “Сумма положительная”, в противном случае - “Сумма отрицательная”;
+     */
+    public static void checkSumSign() {
+        int a = 88;
+        int b = 77;
+        int c = a + b;
+        if (c >= 0) {
+            System.out.println("Сумма положительная");
+        }
+        else  {
+            System.out.println("Сумма отрицательная");
+        }
+    }
 }
+
