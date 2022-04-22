2fb87fa8c52e2a1211a3b9f026e7248e3e987198
MaksimKarpeshin <feap135@yandex.ru>
2022-04-22 16:57:48 +0300

    Task 3

--- src/main/java/ru/gb/karpeshin/homeWork2/homeWorkApp2.java
+++ src/main/java/ru/gb/karpeshin/homeWork2/homeWorkApp2.java
@@ -5,6 +5,8 @@ public class homeWorkApp2 {
         within10and20(1, 5);
         String e = checkNumber(-1);
         System.out.println(e);
+        boolean f = checkPositiveOrNegative(10);  //№3
+        System.out.println(f);
 
     }
 
@@ -24,5 +26,12 @@ public class homeWorkApp2 {
             return "Положительное число";
         else
             return "Отрицательное число";
+    }
+
+    /**
+     * 3. Написать метод, которому в качестве параметра передается целое число. Метод должен вернуть true, если число отрицательное, и вернуть false если положительное.
+     */
+    public static boolean checkPositiveOrNegative(int f) { //№3
+        return (f < 0);
     }
 }
